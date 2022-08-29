c648bbb7511a55463dd9c3b0d8187d5b83bca635
AbdElrahman Gbr <abdelrahman.gbr.os@gmail.com>
2022-08-29 08:59:27 +0200

    implemented CRUD on Json server (Create/Show)

--- src/Todo.js
+++ src/Todo.js
@@ -1,31 +1,75 @@
 import Header from "./Header";
 import {useEffect, useState} from "react";
+import apiRequest from "./apiRequest";
+import AddTodo from "./AddTodo";
 
 function Todo() {
+    const API_URL = "http://localhost:3001/todo-list";
     const [todoList, setTodoList] = useState([]);
-    useEffect(()=>{
-        let url = "http://localhost:3001/todo-list"
-        fetch(url)
-            .then(res => res.json())
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+
+    const addTodo = async (title) => {
+        const id = todoList.length ? todoList[todoList.length - 1].id + 1 : 1;
+        const myNewTodo = {id, title};
+        const listTodos = [...todoList, myNewTodo];
+        setTodoList(listTodos);
+        const postOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(myNewTodo)
+        }
+        const result = await apiRequest(API_URL, postOptions);
+        if (result) setError(result);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!newTodo) return;
+        console.log(newTodo);
+        addTodo(newTodo);
+        setNewTodo('');
+        console.log('submitted');
+    }
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Could not fetch the data for that resource!');
+                }
+                return res.json()
+            })
             .then(data => {
                 // console.log(data);
                 setTodoList(data);
+                setIsPending(false);
+                setError([]);
             })
-    },[]);
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+    }, []);
     console.log(todoList);
+
     return (
         <>
             <Header/>
             <div className={'flex flex-col'}>
                 <div className={'-mt-56 m-auto lg:w-1/2'}>
                     <h1 className={'font-bold text-5xl text-white'}>TODO</h1>
-                    <input placeholder={'Create a new Todo'}
-                           className={'my-10 text-2xl p-4 bg-gray-800 text-white w-full'}></input>
-                    {todoList && todoList.map((todo, index) => {
+                    <AddTodo newTodo={newTodo} setNewTodo={setNewTodo} handleSubmit={handleSubmit} />
+                    {error && <div className={'font-bold text-white'}>{error}</div>}
+                    {isPending && <div className={'text-white'}>Loading Data...</div>}
+                    {todoList && todoList.map((todo, id) => {
                         return (
-                        <div key={index}>
-                        <div className={'text-2xl p-4 bg-gray-800 text-white border-t'}>{todo.title}</div>
-                        </div>
+                            <div key={id}>
+                                <div className={'text-2xl p-4 bg-gray-800 text-white border-t'}>{todo.title}</div>
+                            </div>
                         )
                     })
                     }
